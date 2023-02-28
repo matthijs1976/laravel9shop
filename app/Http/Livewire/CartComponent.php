d10422f3f22086c247214ee6b3388a3773b98622
matthijs1976 <mthijz@gmail.com>
2023-02-28 11:38:22 +0100

    cart is now dynamic, qty+- and delete cart

--- app/Http/Livewire/CartComponent.php
+++ app/Http/Livewire/CartComponent.php
@@ -3,13 +3,35 @@
 namespace App\Http\Livewire;
 
 use Livewire\Component;
+use Cart;
 
 class CartComponent extends Component
 {
-    public function increaseQuantity($rowId)
+    public function incQuantity($rowId)
     {
-        
+        $product = Cart::get($rowId);
+        $qty = $product->qty + 1;
+        Cart::update($rowId,$qty);
     }
+
+    public function decQuantity($rowId)
+    {
+        $product = Cart::get($rowId);
+        $qty = $product->qty - 1;
+        Cart::update($rowId,$qty);
+    }
+
+    public function destroy($id)
+    {
+        Cart::remove($id);
+        session()->flash('success_message','Product Is Verwijderd');
+    }
+
+    public function clearAll()
+    {
+        Cart::destroy();
+    }
+
     public function render()
     {
         return view('livewire.cart-component');
--- resources/views/livewire/cart-component.blade.php
+++ resources/views/livewire/cart-component.blade.php
@@ -35,7 +35,7 @@
                       <div class="py-1"><svg class="fill-current h-6 w-6 text-teal-500 mr-4" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20"><path d="M2.93 17.07A10 10 0 1 1 17.07 2.93 10 10 0 0 1 2.93 17.07zm12.73-1.41A8 8 0 1 0 4.34 4.34a8 8 0 0 0 11.32 11.32zM9 11V9h2v6H9v-4zm0-6h2v2H9V5z"/></svg></div>
                       <div>
                         <p class="font-bold">Success | {{ Session::get('success_message')}} </p>
-                        <p class="text-sm">Make sure you know how these changes affect you.</p>
+                        <p class="text-sm"></p>
                       </div>
                     </div>
                   </div>
@@ -78,20 +78,34 @@
               </div>
   
               <div class="flex flex-1 items-center justify-end gap-2">
+                <button wire:click.prevent="incQuantity('{{ $item->rowId }}')" class="text-gray-600 transition hover:text-red-600">
+                  <span class="sr-only">Add item</span>
+                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-4 h-4">
+                  <path stroke-linecap="round" stroke-linejoin="round" d="M4.5 15.75l7.5-7.5 7.5 7.5" />
+                </svg>
+                </button>
+                <button wire:click.prevent="decQuantity('{{ $item->rowId }}')" class="text-gray-600 transition hover:text-red-600">
+                  <span class="sr-only">Remove item</span>
+                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-4 h-4">
+                  <path stroke-linecap="round" stroke-linejoin="round" d="M19.5 8.25l-7.5 7.5-7.5-7.5" />
+                </svg>
+                </button>
                 <form>
+
+                  
                   <label for="Line1Qty" class="sr-only"> Quantity </label>
-  
+                 
                   <input
                     type="number"
                     min="1"
-                    value="1"
+                    value="{{ $item->qty }}"
                     id="Line1Qty"
                     class="h-8 w-12 rounded border-gray-200 bg-gray-50 p-0 text-center text-xs text-gray-600 [-moz-appearance:_textfield] focus:outline-none [&::-webkit-outer-spin-button]:m-0 [&::-webkit-outer-spin-button]:appearance-none [&::-webkit-inner-spin-button]:m-0 [&::-webkit-inner-spin-button]:appearance-none"
                   />
                 </form>
   
-                <button class="text-gray-600 transition hover:text-red-600">
-                  <span class="sr-only">Remove item</span>
+                <button wire:click.prevent="destroy('{{ $item->rowId}}')"class="text-gray-600 transition hover:text-red-600">
+                  <span class="sr-only">Remove items</span>
   
                   <svg
                     xmlns="http://www.w3.org/2000/svg"
@@ -140,7 +154,7 @@
                 </div>
               </dl>
   
-              <div class="flex justify-end">
+              {{-- <div class="flex justify-end">
                 <span
                   class="inline-flex items-center justify-center rounded-full bg-indigo-100 px-2.5 py-0.5 text-indigo-700"
                 >
@@ -160,9 +174,19 @@
                   </svg>
   
                   <p class="whitespace-nowrap text-xs">2 Discounts Applied</p>
-                </span>
+                  
+                </span>                
+              </div> --}}
+              @if(Cart::count() > 1) 
+              <div class="flex justify-end">
+                <button wire:click.prevent="clearAll()" class="inline-flex items-center justify-center rounded-full bg-red-100 px-2.5 py-0.5 text-blue-600">
+                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
+                  <path stroke-linecap="round" stroke-linejoin="round" d="M14.74 9l-.346 9m-4.788 0L9.26 9m9.968-3.21c.342.052.682.107 1.022.166m-1.022-.165L18.16 19.673a2.25 2.25 0 01-2.244 2.077H8.084a2.25 2.25 0 01-2.244-2.077L4.772 5.79m14.456 0a48.108 48.108 0 00-3.478-.397m-12 .562c.34-.059.68-.114 1.022-.165m0 0a48.11 48.11 0 013.478-.397m7.5 0v-.916c0-1.18-.91-2.164-2.09-2.201a51.964 51.964 0 00-3.32 0c-1.18.037-2.09 1.022-2.09 2.201v.916m7.5 0a48.667 48.667 0 00-7.5 0" />
+                </svg>   
+                  <p class="whitespace-nowrap text-xs">Leeg Winkelmandje</p>                  
+              </button>                
               </div>
-  
+              @endif  
               <div class="flex justify-end">
                 <a
                   href="#"
